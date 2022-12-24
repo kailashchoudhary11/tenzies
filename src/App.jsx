de218ab74d28a9de6c81326d95a0a5968ccb4d2a
kai <kailashjat604@gmail.com>
2022-12-24 19:24:56 +0530

    Roll Dice

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import "./App.css";
 import Die from "./components/Die";
 
 function App() {
+	function allNewDice() {
+		const newDice = [];
+		for (let i = 0; i < 10; i++) {
+			newDice.push(Math.floor(Math.random() * 6) + 1)
+		}
+		return newDice;
+	}
+	const [dice, setDice] = React.useState(allNewDice());
+	const diceComponents = dice.map((die, index) => (
+		<Die value={die} key={index} />
+	))
+
+	function rollDice() {
+		setDice(allNewDice());
+	}
 	return (
 		<main>
 			<div className="dice-container">
-				<Die value={1} className="die-block"/>
-				<Die value={2} className="die-block"/>
-				<Die value={3} className="die-block"/>
-				<Die value={4} className="die-block"/>
-				<Die value={5} className="die-block"/>
-				<Die value={6} className="die-block"/>
-				<Die value={3} className="die-block"/>
-				<Die value={3} className="die-block"/>
-				<Die value={3} className="die-block"/>
-				<Die value={3} className="die-block"/>
+				{diceComponents}
 			</div>
+			<button className="roll-dice" onClick={rollDice}>Roll</button>
 		</main>
 	)
 }
