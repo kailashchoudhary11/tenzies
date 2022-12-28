0b88167c50506b2752af278d77c4f09f7c3dce2b
kai <kailashjat604@gmail.com>
2022-12-28 08:48:21 +0530

    Show number of rolls

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,89 @@
 import React from "react";
 import "./App.css";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from 'react-confetti'
 
 function App() {
+
+	const [dice, setDice] = React.useState(allNewDice());
+	
+	const [tenzies, setTenzies] = React.useState(false);
+
+	const [rolls, setRolls] = React.useState(1);
+
+	const [lowestRolls, setLowestRolls] = React.useState(JSON.parse(localStorage.getItem("lowestRolls")) || Number.MAX_VALUE);
+
+	const diceComponents = dice.map(die => (	
+		<Die 
+			isHeld={die.isHeld} 
+			value={die.value} 
+			key={die.id} 
+			handleClick={() => handleHold(die.id)}
+		/>
+	));
+
+	React.useEffect(() => {
+		const val = dice[0].value;
+		setTenzies(dice.every(die => die.value === val && die.isHeld));
+		gameOver();
+	}, [dice, tenzies]);
+
+	function gameOver() {
+		if (tenzies) {
+			console.log("winner");
+			if (rolls < lowestRolls) {
+				setLowestRolls(rolls);
+				localStorage.setItem("lowestRolls", JSON.stringify(lowestRolls));
+			}
+		}
+	}
+
 	function allNewDice() {
 		const newDice = [];
 		for (let i = 0; i < 10; i++) {
-			newDice.push(Math.floor(Math.random() * 6) + 1)
+			const value = Math.floor(Math.random() * 6) + 1;
+			newDice.push({
+				"value": value, 
+				isHeld: false,
+				id: nanoid(),
+			});
 		}
 		return newDice;
 	}
-	const [dice, setDice] = React.useState(allNewDice());
-	const diceComponents = dice.map((die, index) => (
-		<Die value={die} key={index} />
-	))
+
+	function handleHold(id) {
+		setDice(prevDice => prevDice.map(die => {
+			return die.id === id ? {...die, isHeld : !die.isHeld} : die;
+		}));
+	}
 
 	function rollDice() {
-		setDice(allNewDice());
+		if (tenzies) {
+			setTenzies(false);
+			setDice(allNewDice());
+			setRolls(0);
+			return;
+		}
+		setDice(prevDice => prevDice.map(die => {
+			return die.isHeld ? 
+				die :
+				{...die, value: Math.floor(Math.random() * 6) + 1}
+		}));
+		setRolls(prevRoll => prevRoll + 1);
 	}
+	
 	return (
 		<main>
+			<h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+			{tenzies && <Confetti/>}
+			<p>Number of Rolls: {rolls}</p>
+			{tenzies && <p>Lowest Rolls: {lowestRolls}</p>}
 			<div className="dice-container">
 				{diceComponents}
 			</div>
-			<button className="roll-dice" onClick={rollDice}>Roll</button>
+			<button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
 		</main>
 	)
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,7 +3,10 @@ import "./Die.css";
 
 function Die(props) {
     return (
-        <div className="die-face">
+        <div 
+            className={`die-face ${props.isHeld && "green-bg"}`}
+            onClick={props.handleClick}
+        >
             <h2 className="die-value">{props.value}</h2>
         </div>
     )
